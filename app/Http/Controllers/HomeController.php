7af6a10ccca94f78675cc2c2a834d2dbd1909875
Noman <nomii.uol@gmail.com>
2024-07-09 18:11:40 +0500

    make checkRole Middelware

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,51 +1,30 @@
 <?php
+// app/Http/Controllers/HomeController.php
 
 namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-use Spatie\Permission\Models\Role;
 use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
         $auth = Auth::user();
         $user = User::with('roles')->where('id' , $auth->id)->first();
-        return view('home' , compact('user'));
-    }
-    public function assignRole()
-    {
-        // Find the user with ID 1, or create a new user if it doesn't exist
-        $user = User::find(1);
-
-        if (!$user) {
-            $user = User::create([
-                'name' => 'Super Admin',
-                'email' => 'superadmin@example.com',
-                'password' => bcrypt('password'), // Ensure you hash the password
-            ]);
+        if ($user->hasRole('super-admin')) {
+            return view('admin.dashboard' ,  compact('user')); // Super Admin Dashboard
         }
 
-        // Assign the 'super-admin' role to the user
-        $user->assignRole('super-admin');
+        if ($user->hasRole('admin')) {
+            return view('admin.dashboard' ,  compact('user')); // Admin Dashboard
+        }
 
-        return response()->json(['message' => 'Role assigned successfully'], 200);
+        if ($user->hasRole('user')) {
+            return view('user.dashboard' ,  compact('user')); // User Dashboard
+        }
+
+        return view('home' ,  compact('user')); // Default Dashboard
     }
 }
